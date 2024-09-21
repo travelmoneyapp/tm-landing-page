5181cb90a70df6bbf5600296ddd4a25476062b5e
Ravi Rajendran <me@ravirajendran.fr>
2024-09-21 15:39:21 +0200

    feat: init landing page v1

--- main.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    updateYear();
-};
-
-let updateYear = () => {
-    let date = new Date();
-    document.querySelector(".copyright-year").innerHTML = `${date.getFullYear()}`;
-}
