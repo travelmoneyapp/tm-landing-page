ac745abd67e397a107c5ef35545ecd6a8d3a139b
Ravi Rajendran <me@ravirajendran.fr>
2024-09-22 19:36:38 +0200

    feat(ui): add nav and different sections

--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-import { initializeApp } from 'firebase/app';
-
-const firebaseApp = initializeApp({
-
-});
--- vite.config.js
+++ vite.config.js
@@ -2,41 +2,47 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import path from 'path';
 
-export default defineConfig({
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+    return {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+            },
         },
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: 'modern-compiler'
-            }
-        }
-    },
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: path.resolve(__dirname, '.well-known'),
-                    dest: ''
-                },
-            ],
-        }),
-    ],
-    server: {
-        host: 'localhost',
-        port: 3000
-    },
-    build: {
-        outDir: path.resolve(__dirname, 'dist'),  // Output directory for production build
-        rollupOptions: {
-            output: {
-                assetFileNames: 'assets/[name].[hash][extname]',
-                chunkFileNames: 'assets/[name].[hash].js',
-                entryFileNames: 'assets/[name].[hash].js',
+        css: {
+            postcss: './postcss.config.js',
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler'
+                }
             }
         },
-    },
+        plugins: [
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: path.resolve(__dirname, '.well-known'),
+                        dest: ''
+                    },
+                ],
+            }),
+        ],
+        server: {
+            host: 'localhost',
+            port: 3000
+        },
+        define: {
+            'process.env.NODE_ENV': `"${mode}"`,
+        },
+        build: {
+            outDir: path.resolve(__dirname, 'dist'),  // Output directory for production build
+            rollupOptions: {
+                output: {
+                    assetFileNames: 'assets/[name].[hash][extname]',
+                    chunkFileNames: 'assets/[name].[hash].js',
+                    entryFileNames: 'assets/[name].[hash].js',
+                }
+            },
+        },
+    };
 });
